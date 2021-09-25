5fa79673bcd8eb4babcf3d8432bd37ec5ce1eaf4
alexn34-n <alexn3469@mail.ru>
2021-09-25 21:43:15 +0300

    Initial commit

---
+++ src/main/java/com/alexn/spring/spring/springboot/spring_course_springboot/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.alexn.spring.spring.springboot.spring_course_springboot.service;
+
+
+
+import com.alexn.spring.spring.springboot.spring_course_springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void  saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+}
